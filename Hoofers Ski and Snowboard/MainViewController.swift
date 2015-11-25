6f899fa4fff970b0d25667e751146674984933fa
sheila hnilicka <jshnilicka@wi.rr.com>
2015-11-24 21:24:59 -0600

    Basic layout so far

---
+++ Hoofers Ski and Snowboard/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  Hoofers Ski and Snowboard
+//
+//  Created by Cormick Hnilicka on 11/23/15.
+//  Copyright © 2015 Cormick Hnilicka. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+    override func didReceiveMemoryWarning() {
+        
+    }
+
+}
